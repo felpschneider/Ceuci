1673c9d3622f8618d7caa225b95ff8874d8575d4
unknown <kamillasr15@gmail.com>
2022-07-08 11:40:36 -0300

    feat: construindo model e repository de produto

---
+++ Projeto Spring/Ceuci/src/main/java/br/com/ceuci/model/Produto.java
@@ -0,0 +1,95 @@
+package br.com.ceuci.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotNull
+	@Size(min = 5, max = 100)
+	private String nome;
+
+	@NotNull
+	@Size(min = 10, max = 500)
+	private String descricao;
+
+	@NotNull
+	private String produto;
+
+	private String foto;
+
+	@NotNull
+	@Size(min = 3, max = 50)
+	private String tamanho;
+
+	@NotNull
+	private Double preco;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getProduto() {
+		return produto;
+	}
+
+	public void setProduto(String produto) {
+		this.produto = produto;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(String tamanho) {
+		this.tamanho = tamanho;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+}
